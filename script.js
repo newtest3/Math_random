33992d7ddd30d36c76e89def2261370b2545e7b7
laborant <82711928+newtest3@users.noreply.github.com>
2025-02-04 11:31:13 +0200

    Changed algoritm random

--- script.js
+++ script.js
@@ -9,80 +9,47 @@ let symbols = 'C?!tYdZ93U4%v1lIO5Bo2PnLTqEpRmD@^#(JSMHz08&uKkAQ*X7hyNfVc$)r:xWeG
 
 // let symbols = '9EB4F5827C61A30D';
 
-
-
-changeSymbols.addEventListener('click', ()=>{
-    if(changeSymbols.textContent == 'Change Motorola') {
+changeSymbols.addEventListener('click', () => {
+    if (changeSymbols.textContent == 'Change Motorola') {
         symbols = '9EB4F5827C61A30D';
         changeSymbols.textContent = 'Change Symbols';
         // console.log(symbols);
-    } else if(changeSymbols.textContent == 'Change Symbols') {
+    } else if (changeSymbols.textContent == 'Change Symbols') {
         symbols = 'C?!tYdZ93U4%v1lIO5Bo2PnLTqEpRmD@^#(JSMHz08&uKkAQ*X7hyNfVc$)r:xWeGjbFgiw6|/as';
         changeSymbols.textContent = 'Change Motorola';
         // console.log(symbols);
     }
-    
+
 });
 
 
 let arr = [];
 let arrRandom;
 
-valueKey.addEventListener('mousedown', ()=>{
+valueKey.addEventListener('mousedown', () => {
     answerKey.textContent = '';
     arr = [];
-    clearInterval(arrRandom);
 });
 
-deleteValue.addEventListener('click', ()=>{
+deleteValue.addEventListener('click', () => {
     answerKey.textContent = '';
     arr = [];
-    clearInterval(arrRandom);
 });
 
-random.addEventListener('click', ()=>{
-    if(!isNaN(valueKey.value) && valueKey.value <= 300) {
-        answerKey.textContent = '';
-        arr = [];
-        let numEnd = valueKey.value;
+random.addEventListener('click', () => {
+
+    arr = [];
+    if (!isNaN(valueKey.value) && valueKey.value <= 300) {
+        
+        for (let i = 1; i <= valueKey.value; i++) {
+            arr.push(symbols[randomTest(0, symbols.length - 1)]);
+        }
        
-        // console.log(valueKey.value);
-            arrRandom = setInterval(()=>{
-            let date = new Date();
-                let num = date.getMilliseconds();
-                
-            setTimeout(()=>{
-                
-                    arr.push(symbols[Math.round((num/1000)*((symbols.length-1)-0)+0)]);
-                // console.log(symbols[Math.round((num/1000)*((symbols.length-1)-0)+0)]);
-                // console.log(Math.round((num*num*num)/10**6));
-                
-                
-            },111);
-            if(arr.length != valueKey){
-                preloader.style.display = 'block';
-            }
-            
-            if(arr.length == numEnd) {
-                preloader.style.display = 'none';
-                // answerKey.textContent = arr.join('');
-                // console.log(arr);
-    
-            let shuffleSymbols = [];
-            for (let i = arr.length - 1; i>-1; i--) {
-                shuffleSymbols.push(arr.splice(randomTest(num, 0,arr.length - 1), 1).join(`'`));
-            }       
-            answerKey.textContent = shuffleSymbols.join('');
-            
-            // console.log(shuffleSymbols);
-           
-            clearInterval(arrRandom);
-            }
-        },689);
+        answerKey.textContent = arr.join('');
+
     } else if (isNaN(valueKey.value)) {
         alert('Ну йо-ма-йо, кількість символів вимірюється в числах!!!!!');
     }
-   
 });
 
 
@@ -98,8 +65,8 @@ random.addEventListener('click', ()=>{
 
 
 //-------- Функція рандомних чисел -------------------------------
-function randomTest (num, min, max) {
-    result = Math.round((num/1000)*((max-min)+min));
+function randomTest(min, max) {
+    result = Math.round(Math.random() * ((max - min) + min));
     return result;
 }
 
